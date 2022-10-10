630bb37c8a4665325c686152a2ced0ecb2216509
Ankit Sharma <nez.dev@icloud.com>
2022-10-10 21:23:52 +0530

    init

---
+++ utils/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from .hackattic import requests
+import tempfile
+
+
+def download_file(url, suffix=''):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    with tempfile.NamedTemporaryFile('wb', delete=False, suffix=suffix) as f:
+        for chunk in response.iter_content(512):
+            f.write(chunk)
+
+        return f.name
